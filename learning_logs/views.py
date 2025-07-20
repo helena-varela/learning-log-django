9a26dfd2c51225a495fe5db741694f461269f099
helena-varela <helena.varelakg@gmail.com>
2025-07-20 15:04:27 -0300

    adicionar novas entradas

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
@@ -34,3 +34,20 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Adiciona uma nova entrada."""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+        
+    context = {'topic': topic, 'form': form}    
+    return render(request, 'learning_logs/new_entry.html',context) 
