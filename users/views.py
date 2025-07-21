6bfc27c0be1dec2f65205facea4f7a82dc0b604c
helena-varela <helena.varelakg@gmail.com>
2025-07-21 00:41:59 -0300

    login e logout

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+# Create your views here.
+
+def login_view(request):
+    """Login utilizando um username e uma senha"""
+    if request.method == 'POST':
+        username = request.POST['username']    
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            print('logado com sucesso')
+        else:
+            print("credenciais invalidas")
+
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    """Logout"""
+    logout(request)
+    return render(request, 'users/login.html')
+
+def register_view(request):
+    """Cadastrando um usuário no sistema"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return redirect(reverse('login'))
+
+    return render(request,'users/register.html')
